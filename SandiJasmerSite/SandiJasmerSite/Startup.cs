a0f223e5ba7eca62b1fc97712421b8c8e5163e33
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-01-12 13:33:15 -0800

    Startup updated

--- SandiJasmerSite/SandiJasmerSite/Startup.cs
+++ SandiJasmerSite/SandiJasmerSite/Startup.cs
@@ -32,7 +32,7 @@ namespace TheRockFanPage
             services.AddTransient<IStoriesRepo, StoriesRepo>(); // Repository Interface, Repository Class in the generics
                 
             //context -> DB provider -> into our connection string
-            services.AddDbContext<StoriesContext>(options => options.UseSqlServer(Configuration["ConnectionStrings: ConnectionString"]));
+            services.AddDbContext<StoriesContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:SQLServerConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
