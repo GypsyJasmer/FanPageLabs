86ebd73064def201ff6f427e89c430363c5c9588
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-01-23 12:54:48 -0800

    Created Lab03 Branch, added RegisterVM, Account Contorller, and Register view. Tested the ability to register and it works and takes me to the index/home.

---
+++ SandiJasmerSite/SandiJasmerSite/Models/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRockFanPage.Models
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
