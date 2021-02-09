84fd4cebf401bf2c25a1c95e08ae3aae19cc9f3b
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-02-09 12:21:02 -0800

    Added DM, added list in story model, added pproperty for list and get to retiurn comments.

---
+++ SandiJasmerSite/SandiJasmerSite/Models/VM/CommentVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRockFanPage.Models
+{
+    public class CommentVM
+    {
+        public int MessageID { get; set; } // Identifies the message being reviewed
+        public int Subject { get; set; }
+        public string CommentText { get; set; }
+    }
+}
