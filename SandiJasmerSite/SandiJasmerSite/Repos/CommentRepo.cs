1c2862c517e703ff98d053aec28da6e1ba55e81b
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2021-03-10 17:27:34 -0800

    Finished Lab with comment API. Added comment repos and Controller. Tested in postman running.

---
+++ SandiJasmerSite/SandiJasmerSite/Repos/CommentRepo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using TheRockFanPage.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace TheRockFanPage.Repos
+{
+ 
+    public class CommentRepo : ICommentRepo
+    {
+       
+        private StoriesContext context;     
+
+        public CommentRepo(StoriesContext c)
+        {
+            context = c;
+        }
+
+        public IQueryable<Comment> Comments
+        {
+            get
+            {
+                return context.Comments.Include(comment => comment.Commenter);
+                  
+            }
+        }
+
+        /*************Add & Update Comment******/
+        public void AddComment(Comment comment)
+        {
+            context.Comments.Add(comment);
+            context.SaveChanges(); //always save, and that will put data into the database
+        }
+
+        /*************Get all API******/
+        public List<Comment> GetAllComments()
+        {
+            return context.Comments.ToList();
+        }
+
+
+
+        /*************Get one Comment by ID for API******/
+ 
+        public Comment GetOneComment_byID(int ID)
+        {
+            return context.Comments.Find(ID);
+        }
+
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+
+    }
+
+
+}
---
+++ SandiJasmerSite/SandiJasmerSite/Repos/ICommentRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheRockFanPage.Models;
+using TheRockFanPage.Repos;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheRockFanPage.Repos
+{
+    public interface ICommentRepo
+    {
+        IQueryable<Comment> Comments { get; }  // Read (or retrieve) Story
+
+        //IQueryable<StoryModel> Date { get; }       // retrieve message by date
+
+        void AddComment(Comment comment);  // Create a story
+
+        List<Comment> GetAllComments();
+
+        Comment GetOneComment_byID(int ID);
+
+        public void SaveChanges();
+     
+
+
+    }
+}
